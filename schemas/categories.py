b2f7e16db0df7b85c1d4e03311c3f58751e27d36
Sgmtubo <sgmtubo@gmail.com>
2026-06-28 17:56:47 +0700

    fix schemas

--- schemas/categories.py
+++ schemas/categories.py
@@ -1,6 +1,7 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 class CategorySchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
     id: str
     name: str
 
